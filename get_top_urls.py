e413713ebef1e920899c3e23e918f1c7155ddaf2
Anshul Rao <31268509+anshulrao@users.noreply.github.com>
2017-09-13 22:22:53 +0530

    adding comments

--- get_top_urls.py
+++ get_top_urls.py
@@ -12,11 +12,12 @@ import re
 #keep count of all the links
 link_count=1
 
+#crawl google search results for given search variable=val and that too upto 'page_count' no. of pages
 def crawlGoogle(val, page_count):
 	query=val.strip().split()
 	query="+".join(query)
 	global link_count
-	for count in range(page_count):		
+	for count in range(page_count):	
 		html = "https://www.google.co.in/search?site=&source=hp&q="+query+"&gws_rd=ssl"+"&start="+str(count)
 		req = urllib2.Request(html, headers={'User-Agent': 'Mozilla/5.0'})
 		soup = BeautifulSoup(urlopen(req).read(),"html.parser")
@@ -31,5 +32,5 @@ def crawlGoogle(val, page_count):
 			link_count+=1
 		count+=10
 
-x=input()
-crawlGoogle(str(x), 5)
+company_name=input()
+crawlGoogle(str(company_name), 5)
